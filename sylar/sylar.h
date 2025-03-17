52cb53f11b47c81e8b48c71d30bc7d7d5efcd05d
reversedog <reversedog69@gmail.com>
2025-03-17 23:21:41 +0800

    http parser,session,servlet,server

--- sylar/sylar.h
+++ sylar/sylar.h
@@ -2,23 +2,40 @@
 #define __SYLAR_SYLAR_H__
 
 #include "address.h"
+#include "bytearray.h"
 #include "config.h"
 #include "endian.h"
 #include "env.h"
 #include "fd_manager.h"
 #include "fiber.h"
 #include "hook.h"
+#include "http/http.h"
+#include "http/http_parser.h"
+#include "http/http_session.h"
+#include "http/http_server.h"
+#include "http/parser11/httpclient_parser.h"
+#include "http/parser11/http11_common.h"
+#include "http/parser11/http11_parser.h"
+#include "http/servlet.h"
+// #include "http/servlets/config_servlet.h"
+// #include "http/servlets/status_servlet.h"
 #include "iomanager.h"
 #include "macro.h"
-#include "scheduler.h"
-#include "socket.h"
-#include "thread.h"
-#include "timer.h"
-
-#include "log.h"
-
 #include "mutex.h"
 #include "noncopyable.h"
-
+#include "scheduler.h"
 #include "singleton.h"
+#include "socket.h"
+#include "stream.h"
+#include "streams/socket_stream.h"
+#include "tcp_server.h"
+#include "thread.h"
+#include "timer.h"
+#include "util.h"
+#include "util/json_util.h"
+#include "log.h"
+
+
+
+
 #endif
